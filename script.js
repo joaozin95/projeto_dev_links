8dfe05659c98852d53f24955ccff14b122e99679
João Gabriel S. Xavier <32635@pmar.corp>
2025-11-04 16:52:39 -0300

    correção de bug

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function toggleMode() {
   // pegar a tag img
   const img = document.querySelector("#profile-img")
 
-  //substituir a imagem
-  if (html.classList.contains("light")) {
-    //se tiver a classe light, troca a imagem
-    img.setAttribute("src", "./assets/avatar-light.png")
+ if (html.classList.contains("light")) {
+    // substitui pela imagem do modo claro
+    img.setAttribute("src", "assets/avatar-light.png")
   } else {
-    //se não tiver a classe light, mantém a imagem original
-    img.setAttribute("src", "./assets/avatar.png")
+    // imagem padrão
+    img.setAttribute("src", "assets/avatar.png")
   }
 }
